af0e689a584e811bbdd802b6fe6b207500175077
David Freifeld <freifeld.david@gmail.com>
2020-08-15 22:07:01 -0700

    Boilerplate main loop

--- vm.c
+++ vm.c
@@ -2,6 +2,8 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 // Define registers
 enum
@@ -37,3 +39,47 @@ enum
     FL_ZRO = 1 << 1,
     FL_NEG = 1 << 2
 };
+
+// Staying 16 bit for now
+uint16_t memory[UINT16_MAX+1];
+uint16_t reg[R_COUNT];
+
+// Simple file size detector via https://stackoverflow.com/questions/8236/how-do-you-determine-the-size-of-a-file-in-c
+off_t fsize(const char *filename) {
+    struct stat st; 
+    if (stat(filename, &st) == 0)
+        return st.st_size;
+    return -1; 
+}
+
+int main(int argc, char** argv)
+{
+    if (argc < 2) {
+        printf("Invalid command: need binary file input!\n");
+        return 1;
+    }
+    FILE* binary = fopen(argv[1], "r");
+    reg[R_PC] = 0x0000; // init program counter
+    for (int i = 0; i < fsize(argv[1]); i++) {
+        reg[R_PC]++;
+        uint16_t op;
+        fread(&op, 2, 1, binary);
+        switch (op) {
+        case OP_ADD:
+            break;
+        case OP_LDR:
+            break;
+        case OP_STR:
+            break;
+        case OP_AND:
+            break;
+        case OP_NOT:
+            break;
+        case OP_JMP:
+            break;
+        default:
+            printf("Bad opcode at %x! Skipping...", i);
+            break;
+        }
+    }
+}
